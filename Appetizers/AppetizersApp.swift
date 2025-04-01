5cc6614c45f712b5524dd562fe6aaf175268deb3
Бойко Роман Олегович <r.boiko@aitiguru.com>
2025-04-01 21:50:27 +0300

    Initial commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Роман Бойко on 01.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
